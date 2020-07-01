32aaade5e0edadbcd1c5628ba496ff4a8b944483
MongieLee <mongielee@163.com>
2020-07-01 23:51:51 +0800

    暂存代码

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,6 +1,11 @@
-import {myAjax} from '../../lib/http'
+import {
+  myHttp
+} from '../../lib/http'
 // const { http }  = require('../../lib/http.js')
-const { app_id, app_secret } = getApp().globalData
+const {
+  app_id,
+  app_secret
+} = getApp().globalData
 
 Page({
   data: {
@@ -8,32 +13,28 @@ Page({
   },
   //点击按钮 => 调用小程序原生的 wx.login => 参数 => http.post => 返回 user
   // => 保存user => 返回首页
-  login(event){
-    console.log(wx.getStorageSync('X-token'))
-    let encrypted_data = event.detail.encryptedData
-    let iv = event.detail.iv
-    this.wxLogin(encrypted_data, iv)
-  },
-  wxLogin(encrypted_data, iv){
+  login(event) {
+    const encrypted_data = event.detail.encryptedData
+    const iv = event.detail.iv
+    let code
     wx.login({
-      success: (res)=> this.loginMe(res.code , iv, encrypted_data)
+      success: (response) => {
+        code = response.code
+        myHttp.post('/sign_in/mini_program_user',{
+          code,
+          iv,
+          encrypted_data,
+          app_id,
+          app_secret,
+        })
+        console.log(111)
+      }
     })
   },
-  loginMe(code, iv, encrypted_data){
-    myAjax.post('/sign_in/mini_program_user', {
-      code,
-      iv,
-      encrypted_data,
-      app_id,
-      app_secret,
-    })
-    .then(response => {
-      this.saveMessage(response)
-      wx.reLaunch({ url: "/pages/home/home" })
+  wxLogin(encrypted_data, iv) {
+    wx.login({
+      success: (res) => this.loginMe(res.code, iv, encrypted_data)
     })
   },
-  saveMessage(response){
-    wx.setStorageSync('me', response.data.resource)
-    wx.setStorageSync('X-token', response.header["X-token"])
-  }
+ 
 })
