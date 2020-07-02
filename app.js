0a8bef75fa910523e64366dc2267f42fe5ddc111
MongieLee <mongielee@163.com>
2020-07-02 09:35:06 +0800

    删除app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-      host: 'https://gp-server.hunger-valley.com',
-      app_id: "wxaf4b1037b2e9952f ",
-      app_secret: "b0613f923d845f8b333c0e474f77e8ab",
-      t_app_id: "6JZNHawtKU9Y47Cp8f5c7WHs",
-      t_app_secret: "taQJp4dqkvMrmDW4819LFr6y"
-  }
- })
